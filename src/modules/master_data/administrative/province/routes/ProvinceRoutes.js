6ddef9b6628cfbbec0eb51a43accc7d629941185
Fikri Ramadhan <fikriramadhan002@gmail.com>
2024-07-19 10:55:27 +0700

    fixing router

--- src/modules/master_data/administrative/province/routes/ProvinceRoutes.js
+++ src/modules/master_data/administrative/province/routes/ProvinceRoutes.js
@@ -6,9 +6,9 @@ const validate = require(process.cwd() + '/src/validations/common/validate')
 const router = express.Router();
 router.post('/create-province', validate(ProvinceRules.create), ProvinceController.CreateProvince);
 router.get("/get-all-province", ProvinceController.GetAllProvince);
-router.delete('/delete-province/:id', ProvinceController.DeleteProvince);
-router.put('/update-province/:id', ProvinceController.UpdateProvince);
-router.get('/get-province/:id', ProvinceController.GetProvince);
+router.delete('/delete-province', ProvinceController.DeleteProvince);
+router.put('/update-province', ProvinceController.UpdateProvince);
+router.get('/get-province', ProvinceController.GetProvince);
 
 module.exports = router;
 
